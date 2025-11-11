bd5b8430d0c02065313dd3dd10dc1c9dd35f70ed
Sujit Malde <samman30@live.com>
2025-11-11 21:19:07 +0000

    Formatting changes & added IOCTL to clear the LIFO

--- procfs_lifo/procfs_lifo.c
+++ procfs_lifo/procfs_lifo.c
@@ -1,15 +1,15 @@
-#include <linux/kernel.h>
-#include <linux/init.h>
-#include <linux/module.h>
-#include <linux/fs.h>
-#include <linux/uaccess.h>
-#include <linux/slab.h>
-#include <linux/proc_fs.h>
-#include <linux/err.h>
-#include <linux/minmax.h>
-#include <linux/string.h>
-#include <linux/mutex.h>
 #include "procfs_lifo_meminfo.h"
+#include <linux/err.h>
+#include <linux/fs.h>
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/minmax.h>
+#include <linux/module.h>
+#include <linux/mutex.h>
+#include <linux/proc_fs.h>
+#include <linux/slab.h>
+#include <linux/string.h>
+#include <linux/uaccess.h>
 
 static int __init procfs_lifo_init(void);
 static void __exit procfs_lifo_exit(void);
@@ -21,13 +21,11 @@ static ssize_t procfs_lifo_write(struct file *file, const char *buf, size_t len,
 static long procfs_lifo_ioctl(struct file *file, unsigned int cmd, unsigned long arg);
 
 static struct proc_dir_entry *parent;
-static struct proc_ops fops = {
-    .proc_open = procfs_lifo_open,
-    .proc_release = procfs_lifo_release,
-    .proc_read = procfs_lifo_read,
-    .proc_write = procfs_lifo_write,
-    .proc_ioctl = procfs_lifo_ioctl
-};
+static struct proc_ops fops = {.proc_open = procfs_lifo_open,
+                               .proc_release = procfs_lifo_release,
+                               .proc_read = procfs_lifo_read,
+                               .proc_write = procfs_lifo_write,
+                               .proc_ioctl = procfs_lifo_ioctl};
 
 /* LIFO data structure */
 
@@ -64,7 +62,7 @@ static ssize_t procfs_lifo_read(struct file *file, char __user *buf, size_t len,
     int i = 0;
 
     if (len > procfs_lifo_meminfo.len) {
-	len = procfs_lifo_meminfo.len;
+        len = procfs_lifo_meminfo.len;
     }
 
     start = procfs_lifo_meminfo.len - len;
@@ -75,37 +73,38 @@ static ssize_t procfs_lifo_read(struct file *file, char __user *buf, size_t len,
 
     char *buff = (char *)kzalloc(len, GFP_KERNEL);
     if (!buff) {
-	pr_err("procfs_lifo couldn't allocate space for user read()\n");
-	return -ENOMEM;
+        pr_err("procfs_lifo couldn't allocate space for user read()\n");
+        return -ENOMEM;
     }
 
     list_for_each_entry(node, &head_node, list) {
-    	if (i == first_node) {
+        if (i == first_node) {
             if (first_node == last_node) {
-		memcpy(buff, node->data + relative_start, relative_end - relative_start);
-		copied += relative_end - relative_start;
-		break;
-	    } else {
-	        memcpy(buff, node->data + relative_start, PROCFS_LIFO_CHUNK_SIZ - relative_start);
-	        copied += PROCFS_LIFO_CHUNK_SIZ - relative_start;
-	    }
-	}
-	     
-	if (i > first_node) {
-	    if (list_is_last(&node->list, &head_node)) {
+                memcpy(buff, node->data + relative_start, relative_end - relative_start);
+                copied += relative_end - relative_start;
+                break;
+            } else {
+                memcpy(buff, node->data + relative_start, PROCFS_LIFO_CHUNK_SIZ - relative_start);
+                copied += PROCFS_LIFO_CHUNK_SIZ - relative_start;
+            }
+        }
+
+        if (i > first_node) {
+            if (list_is_last(&node->list, &head_node)) {
                 used = PROCFS_LIFO_CHUNK_SIZ - (procfs_lifo_meminfo.total_alloc_bytes - procfs_lifo_meminfo.len);
-		memcpy(buff + copied, node->data, used);
-		copied += used;
-	    } else {
-		memcpy(buff + copied, node->data, PROCFS_LIFO_CHUNK_SIZ);
-		copied += PROCFS_LIFO_CHUNK_SIZ;
-	    }
-	}
-	i++;
+                memcpy(buff + copied, node->data, used);
+                copied += used;
+            } else {
+                memcpy(buff + copied, node->data, PROCFS_LIFO_CHUNK_SIZ);
+                copied += PROCFS_LIFO_CHUNK_SIZ;
+            }
+        }
+
+        i++;
     }
-    
+
     if (copy_to_user(buf, buff, len)) {
-	pr_warn("procfs_lifo read() did not copy_to_user all bytes\n");
+        pr_warn("procfs_lifo read() did not copy_to_user all bytes\n");
     }
 
     return copied;
@@ -114,8 +113,8 @@ static ssize_t procfs_lifo_read(struct file *file, char __user *buf, size_t len,
 static ssize_t procfs_lifo_write(struct file *file, const char *buf, size_t len, loff_t *off) {
 
     if (mutex_lock_interruptible(&procfs_lifo_mutex)) {
-	pr_err("procfs_lifo write() couldn't lock mutex\n");
-	return -1;
+        pr_err("procfs_lifo write() couldn't lock mutex\n");
+        return -1;
     }
 
     struct node *node;
@@ -127,37 +126,37 @@ static ssize_t procfs_lifo_write(struct file *file, const char *buf, size_t len,
         unused = procfs_lifo_meminfo.total_alloc_bytes - procfs_lifo_meminfo.len;
         to_copy = min(unused, len);
         list_for_each_entry(node, &head_node, list) {
-	    if (list_is_last(&node->list, &head_node)) {
-	        if (copy_from_user(node->data, buf, to_copy)) {
-		    pr_err("procfs_lifo couldn't copy_from_user\n");
-		    goto err;
-		}
+            if (list_is_last(&node->list, &head_node)) {
+                if (copy_from_user(node->data, buf, to_copy)) {
+                    pr_err("procfs_lifo couldn't copy_from_user\n");
+                    goto err;
+                }
 
-	        procfs_lifo_meminfo.len += to_copy;
-		total_copied += to_copy;
-	    }
+                procfs_lifo_meminfo.len += to_copy;
+                total_copied += to_copy;
+            }
         }
     }
 
     while (total_copied != len) {
-	to_copy = min(PROCFS_LIFO_CHUNK_SIZ, len - total_copied);
-	new_node = (struct node *)kzalloc(sizeof(struct node), GFP_KERNEL);
-	if (!new_node) {
-	    pr_err("procfs_lifo couldn't allocate new linked list node\n");
-	    goto err;
-	}
+        to_copy = min(PROCFS_LIFO_CHUNK_SIZ, len - total_copied);
+        new_node = (struct node *)kzalloc(sizeof(struct node), GFP_KERNEL);
+        if (!new_node) {
+            pr_err("procfs_lifo couldn't allocate new linked list node\n");
+            goto err;
+        }
 
-	procfs_lifo_meminfo.total_alloc_bytes += PROCFS_LIFO_CHUNK_SIZ;
-	INIT_LIST_HEAD(&new_node->list);
-	list_add_tail(&new_node->list, &head_node);
-	procfs_lifo_meminfo.nodes++;
-	if (copy_from_user(new_node->data, buf + total_copied, to_copy)) {
-	    pr_err("procfs_lifo couldn't copy from user\n");
-	    goto err;
-	}
+        procfs_lifo_meminfo.total_alloc_bytes += PROCFS_LIFO_CHUNK_SIZ;
+        INIT_LIST_HEAD(&new_node->list);
+        list_add_tail(&new_node->list, &head_node);
+        procfs_lifo_meminfo.nodes++;
+        if (copy_from_user(new_node->data, buf + total_copied, to_copy)) {
+            pr_err("procfs_lifo couldn't copy from user\n");
+            goto err;
+        }
 
-	procfs_lifo_meminfo.len += to_copy;
-	total_copied += to_copy;
+        procfs_lifo_meminfo.len += to_copy;
+        total_copied += to_copy;
     }
 
     mutex_unlock(&procfs_lifo_mutex);
@@ -170,28 +169,41 @@ err:
 
 static long procfs_lifo_ioctl(struct file *file, unsigned int cmd, unsigned long arg) {
     switch (cmd) {
-	case GET_STATS:
-	    if (copy_to_user((struct procfs_lifo_meminfo_t *)arg, &procfs_lifo_meminfo, sizeof(procfs_lifo_meminfo))) {
-	        pr_err("procfs_lifo_ioctl failure\n");
-	    }
-	    break;
-	default:
-	    break;
+    case GET_STATS:
+        if (copy_to_user((struct procfs_lifo_meminfo_t *)arg, &procfs_lifo_meminfo, sizeof(procfs_lifo_meminfo))) {
+            pr_err("procfs_lifo_ioctl failure\n");
+        }
+
+        break;
+
+    case CLEAR_LIFO: {
+        struct node *node, *temp;
+        list_for_each_entry_safe(node, temp, &head_node, list) {
+            list_del(&node->list);
+            kfree(node);
+        }
+
+        memset(&procfs_lifo_meminfo, 0, sizeof(procfs_lifo_meminfo));
+        break;
+    }
+
+    default:
+        break;
     }
 
     return 0;
 }
 
 static int __init procfs_lifo_init(void) {
-    parent = proc_mkdir("sm_canonical", NULL);
+    parent = proc_mkdir("suj", NULL);
     if (!parent) {
-	    pr_err("Error creating proc parent directory\n");
-    	    return -1;
+        pr_err("Error creating proc parent directory\n");
+        return -1;
     }
 
     if (!proc_create("procfs_lifo", 0666, parent, &fops)) {
-	pr_err("Error creating proc file\n");
-	goto r_parent;
+        pr_err("Error creating proc file\n");
+        goto r_parent;
     }
 
     pr_info("procfs_lifo loaded successfully\n");
--- procfs_lifo/procfs_lifo_meminfo.h
+++ procfs_lifo/procfs_lifo_meminfo.h
@@ -1,4 +1,4 @@
-#define GET_STATS  _IOR('a', 'a', struct procfs_lifo_meminfo_t *)
+#define GET_STATS _IOR('a', 'a', struct procfs_lifo_meminfo_t *)
 #define CLEAR_LIFO _IOW('a', 'b', struct procfs_lifo_meminfo_t *)
 
 #define PROCFS_LIFO_CHUNK_SIZ 1024
@@ -8,4 +8,3 @@ struct procfs_lifo_meminfo_t {
     uint64_t len;
     uint16_t nodes;
 };
-
