221f9fadf26e597cd31c089cdeab10b82bbe2e6e
Sujit Malde <sujitmalde@yahoo.co.uk>
2025-11-15 22:27:11 +0000

    Better setup for each framework instance

--- procfs_lifo/procfs_lifo_bmark.cpp
+++ procfs_lifo/procfs_lifo_bmark.cpp
@@ -4,17 +4,24 @@
 #include <memory>
 #include <random>
 #include <stdexcept>
+#include <sys/ioctl.h>
 #include <unistd.h>
 
-template <typename T, size_t S>
-class ProcfsLifoTester : public benchmark::Fixture {
+#include "kernel/procfs_lifo_meminfo.h"
+
+// TODO: ClearLifo, 2x std::systems on each iteration
+
+template <typename T, size_t S> class ProcfsLifoTester : public benchmark::Fixture {
   public:
     void SetUp(const benchmark::State &state) override {
         bool prewrite{state.range(0) == 1 ? true : false};
         if ((fd_ = open("/proc/suj/procfs_lifo", O_RDWR)) < 0) {
-			std::system("rmmod procfs_lifo");
             throw std::runtime_error("Couldn't open proc file");
         }
+
+		ClearLifo();
+        std::system("sudo su -c 'echo 1 > /proc/sys/vm/drop_caches'");
+        std::system("sudo su -c 'echo 1 > /proc/sys/vm/compact_memory'");
 
         userbuf_ = std::make_unique<std::array<T, S>>();
         if (prewrite) {
@@ -23,32 +30,34 @@ class ProcfsLifoTester : public benchmark::Fixture {
         }
     }
 
-    void TearDown(const benchmark::State &state) override {
-        close(fd_);
-    }
+    void TearDown(const benchmark::State &state) override { close(fd_); }
 
-    void WriteLifo() { write(fd_, userbuf_->data(), S*sizeof(T)); }
-    void ReadLifo() { read(fd_, userbuf_->data(), S*sizeof(T)); }
+    void WriteLifo() { write(fd_, userbuf_->data(), S * sizeof(T)); }
+    void ReadLifo() { read(fd_, userbuf_->data(), S * sizeof(T)); }
 
   private:
     void FillUserbufRandom() {
         std::mt19937 mt{std::random_device{}()};
         std::uniform_int_distribution<T> dst{0, 255};
         for (size_t i{0}; i < S; ++i) {
-			(*userbuf_)[i] = dst(mt);
+            (*userbuf_)[i] = dst(mt);
         }
     }
+
+	void ClearLifo() {
+		::ioctl(fd_, CLEAR_LIFO);
+	}
 
     int fd_;
     std::unique_ptr<std::array<T, S>> userbuf_;
 };
 
-BENCHMARK_TEMPLATE_DEFINE_F(ProcfsLifoTester, WriteBench10000, uint8_t, 10000)(benchmark::State& state) {
-	for (auto _ : state) {
-		this->WriteLifo();
-		benchmark::ClobberMemory();
-	}
+BENCHMARK_TEMPLATE_DEFINE_F(ProcfsLifoTester, WriteBench10000, uint8_t, 10000)(benchmark::State &state) {
+    for (auto _ : state) {
+        this->WriteLifo();
+        benchmark::ClobberMemory();
+    }
 }
-BENCHMARK_REGISTER_F(ProcfsLifoTester, WriteBench10000)->Arg(0)->ThreadRange(1, 1);
+BENCHMARK_REGISTER_F(ProcfsLifoTester, WriteBench10000)->Arg(0)->ThreadRange(1, 1)->Iterations(1000);
 
 BENCHMARK_MAIN();
